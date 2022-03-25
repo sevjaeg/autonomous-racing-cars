8f2675acb1775b1fd4fadc5f3a1c3af8dbc23cf3
sevjaeg <sev.jaeger@gmail.com>
2022-03-25 19:41:38 +0100

    c++ safety node improved

--- lab2/safety_node1/src/safety_node.cpp
+++ lab2/safety_node1/src/safety_node.cpp
@@ -4,7 +4,7 @@
 
 #include <iostream>
 
-// TODO: include ROS msg type headers and libraries
+// Include ROS msg type headers and libraries
 #include <std_msgs/Bool.h>
 #include <ackermann_msgs/AckermannDriveStamped.h>
 #include <algorithm>
@@ -15,7 +15,11 @@ class Safety {
 private:
     ros::NodeHandle n;
     double speed;
-    // TODO: create ROS subscribers and publishers
+    double threshold_fw;
+    double threshold_bw;
+    double threshold;
+
+    // Create ROS subscribers and publishers
     ros::Subscriber sub_scan_;
     ros::Subscriber sub_odom_;
     ros::Publisher pub_brake_;
@@ -42,45 +46,55 @@ public:
         NOTE that the x component of the linear velocity in odom is the speed
         */
 
-        // TODO: create ROS subscribers and publishers
+        // Create ROS subscribers and publishers
         sub_scan_ = n.subscribe("scan", 10, &Safety::scan_callback, this);
         sub_odom_ = n.subscribe("odom", 10, &Safety::odom_callback, this);
 
         pub_brake_ = n.advertise<ackermann_msgs::AckermannDriveStamped>("brake", 10);
         pub_brake_bool_ = n.advertise<std_msgs::Bool>("brake_bool", 10);
+
+        if(n.getParam("/safety_node/threshold_fw", threshold_fw) && n.getParam("/safety_node/threshold_bw", threshold_bw)) {
+            ROS_INFO("Got TTC thresholds fw:%f, bw:%f", threshold_fw, threshold_bw);
+        } else {
+            ROS_ERROR("Failed to get TTC thresholds params");
+        }
     }
     void odom_callback(const nav_msgs::Odometry::ConstPtr &odom_msg) {
-        // TODO: update current speed
+        // Update current speed and TTC threshold
         speed = odom_msg->twist.twist.linear.x;
-//         ROS_INFO("speed info : %f", speed);
+        if(speed < 0.0) {
+            threshold = threshold_bw;
+        } else {
+            threshold = threshold_fw;
+        }
     }
 
     void scan_callback(const sensor_msgs::LaserScan::ConstPtr &scan_msg) {
-        // TODO: calculate TTC
         int length = scan_msg->ranges.size();
         double* ttc = new double[length];
-        double threshold = 2
-        ;
 
+        // Calculate TTC
         for (int i = 0; i < length; i++) {
             if (scan_msg->ranges[i] < scan_msg->range_min || scan_msg->ranges[i] > scan_msg->range_max) {
-                ttc[i] = -1.0;
+                ttc[i] = std::numeric_limits<double>::infinity();
                 continue;
             }
 
             double angle = scan_msg->angle_min + scan_msg->angle_increment * i;
-            double r_derivative = speed * cos(angle); 
+            double r_derivative = -speed * cos(angle); 
             ttc[i] = scan_msg->ranges[i] / std::max(-r_derivative, 0.0);
-            ROS_INFO("ttc[%d]=%f", i, ttc[i]);
+
+            ROS_DEBUG("range %f at angle %f at speed %f yields ttc %f", scan_msg->ranges[i], angle, r_derivative, ttc[i]);
         }
         
         double* min = std::min_element(ttc, ttc + length);
+        ROS_DEBUG("min ttc=%f", *min);
         
-        // TODO: publish drive/brake message
+        // Publish drive/brake message
         std_msgs::Bool brake_bool_msg;
         if (*min < threshold && *min > 0) {
-            ROS_INFO("threshold warning %f", *min);
-            
+            ROS_INFO("threshold warning with ttc=%f", *min);
+
             ackermann_msgs::AckermannDriveStamped brake_msg;
             brake_msg.drive.speed = 0.0;
             pub_brake_.publish(brake_msg);
@@ -90,21 +104,19 @@ public:
             brake_bool_msg.data = false;
         }
         pub_brake_bool_.publish(brake_bool_msg);
-
-
     }
-
 };
+
 int main(int argc, char ** argv) {
     ros::init(argc, argv, "safety_node");
     Safety sn;
+
     ROS_INFO("Hello from safety_node!");
 
-    ros::Rate loop_rate(1);
+    ros::Rate loop_rate(40);
 
     while (ros::ok())
     {
-//         ROS_INFO("safety_node looping");
         ros::spinOnce();
         loop_rate.sleep();
     }
