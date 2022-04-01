b6235b5ec74aea3b59226ef656b6cbcf6cecd7bd
JackieJoker <danilocastiglia@hotmail.it>
2022-04-01 12:44:07 +0200

    Slow Crash Node implemented

--- lab3/slow_crash/scripts/slow_crash.py
+++ lab3/slow_crash/scripts/slow_crash.py
@@ -9,49 +9,18 @@ import rospy
 from sensor_msgs.msg import Image, LaserScan
 from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
 
-#PID CONTROL PARAMS
-kp = #TODO
-kd = #TODO
-ki = #TODO
-servo_offset = 0.0
-prev_error = 0.0 
-error = 0.0
-integral = 0.0
-
-#WALL FOLLOW PARAMS
-ANGLE_RANGE = 270 # Hokuyo 10LX has 270 degrees scan
-DESIRED_DISTANCE_RIGHT = 0.9 # meters
-DESIRED_DISTANCE_LEFT = 0.55
-VELOCITY = 2.00 # meters per second
-CAR_LENGTH = 0.50 # Traxxas Rally is 20 inches or 0.5 meters
-
-class WallFollow:
-    """ Implement Wall Following on the car
-    """
+class SlowCrash:
     def __init__(self):
-        #Topics & Subs, Pubs
+        # Topics & Subs, Pubs
         lidarscan_topic = '/scan'
         drive_topic = '/nav'
 
-        self.lidar_sub = #TODO: Subscribe to LIDAR
-        self.drive_pub = #TODO: Publish to drive
-
-    def getRange(self, data, angle):
-        # data: single message from topic /scan
-        # angle: between -45 to 225 degrees, where 0 degrees is directly to the right
-        # Outputs length in meters to object with angle in lidar scan field of view
-        #make sure to take care of nans etc.
-        #TODO: implement
-        return 0.0
+        self.lidar_sub = rospy.Subscriber(lidarscan_topic, LaserScan, self.lidar_callback)
+        self.drive_pub = rospy.Publisher(drive_topic, AckermannDriveStamped, queue_size=10)
 
     def pid_control(self, error, velocity):
-        global integral
-        global prev_error
-        global kp
-        global ki
-        global kd
         angle = 0.0
-        #TODO: Use kp, ki & kd to implement a PID controller for 
+
         drive_msg = AckermannDriveStamped()
         drive_msg.header.stamp = rospy.Time.now()
         drive_msg.header.frame_id = "laser"
@@ -59,21 +28,15 @@ class WallFollow:
         drive_msg.drive.speed = velocity
         self.drive_pub.publish(drive_msg)
 
-    def followLeft(self, data, leftDist):
-        #Follow left wall as per the algorithm 
-        #TODO:implement
-        return 0.0 
-
     def lidar_callback(self, data):
-        """ 
-        """
-        error = 0.0 #TODO: replace with error returned by followLeft
+        VELOCITY = 0.1
+        error = 0.0
         #send error to pid_control
         self.pid_control(error, VELOCITY)
 
 def main(args):
-    rospy.init_node("WallFollow_node", anonymous=True)
-    wf = WallFollow()
+    rospy.init_node("SlowCrash_node", anonymous=True)
+    sc = SlowCrash()
     rospy.sleep(0.1)
     rospy.spin()
 
