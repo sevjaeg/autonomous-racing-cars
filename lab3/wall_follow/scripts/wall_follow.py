ba2d700e8a9d91b3f89e54352f3deed0174223f5
sevjaeg <sev.jaeger@gmail.com>
2022-04-19 11:12:30 +0200

    code cleanup, small optimisations
    lab time 34.9s

--- lab3/wall_follow/scripts/wall_follow.py
+++ lab3/wall_follow/scripts/wall_follow.py
@@ -3,11 +3,10 @@ from __future__ import print_function
 from re import T
 import sys
 import math
-from turtle import speed
+from turtle import left, speed
 
 #ROS Imports
 import rospy
-import tf
 from std_msgs.msg import Float64
 from sensor_msgs.msg import LaserScan
 from ackermann_msgs.msg import AckermannDriveStamped
@@ -33,49 +32,49 @@ USE_DYNAMIC_RECONFIG = False
 if USE_DYNAMIC_RECONFIG:
     from wall_follow.cfg import GainsConfig
 
-#PID CONTROL PARAMS
+# PID CONTROL PARAMS
 kp = 0.32
 kd = 0.0015
-ki = 0.0
+ki = 0
 
-#WALL FOLLOW PARAMS
-theta = 42 # degrees
-desired_distance_left = 1.5 # meters (only active if no dynamic distance)
-
-# Dynamic params
-dynamic_distance = True
+# WALL FOLLOW PARAMS
+THETA = 42 # degrees
+DYNAMIC_DISTANCE = False  # drives in the middle of the track
+DESIRED_DISTANCE_LEFT = 1.5 # meters (only active if no dynamic distance)
+MAX_WALL_DISTANCE = 1.8  # m (only active with dynamic distance, helps with wide curves)
+BASIC_VELOCITY = True  # simple velocity scheme from the assignment sheet, otherwise more aggressive behaviour
+MAX_SPEED = 6  # m/s  (only without basic velocity)
+MIN_SPEED = 1.7  # m/s  (only without basic velocity)
+LOOKAHEAD_DIST_FAST = 3.0  # m  (if the car drives more than 5 m/s)
+LOOKAHEAD_DIST_MID = 2.25  # m  (if the car drives more than 3 m/s)
+LOOKAHEAD_DIST_SLOW = 1.5  # m  (if the car drives slower than 3 m/s)
 
 # Car params
 MAX_STEERING_ANGLE = math.radians(24)
-MAX_SPEED = 5.8  # m/s
-MIN_SPEED = 1.7  # m/s
-# 1.4 for levine map
-MAX_WALL_DISTANCE = 1.8  # m 
 
-servo_offset = 0.0
+# Globar variables
 prev_error = 0.0 
 error = 0.0
 integral = 0.0
 prev_time = 0.0
 velocity = 0.0
-ttc = 0.0
 
 def reconfig_callback(config, level):
     global kp
     global ki
     global kd
-    global desired_distance_left
-    global theta
-    global dynamic_distance
+    global DESIRED_DISTANCE_LEFT
+    global THETA
+    global DYNAMIC_DISTANCE
     kp = config.kp
     ki = config.ki
     kd = config.kd
-    desired_distance_left = config.dist
-    theta = config.theta
-    dynamic_distance = config.dynamic_dist
+    DESIRED_DISTANCE_LEFT = config.dist
+    THETA = config.THETA
+    DYNAMIC_DISTANCE = config.dynamic_dist
     rospy.loginfo("Gains set to kp={kp}, ki={ki}, kd={kd}".format(**config))
     rospy.loginfo("Wall distance set to {dist} m".format(**config))
-    rospy.loginfo("Theta set to {theta} degrees".format(**config))
+    rospy.loginfo("Theta set to {THETA} degrees".format(**config))
     rospy.loginfo("Lookahead distance set to {d_lookahead} m".format(**config))
     return config
 
@@ -101,10 +100,7 @@ class WallFollow:
         self.dist_left_pub = rospy.Publisher(dist_topic, Float64, queue_size=10)
         self.dist_lookahead_pub = rospy.Publisher(dist_lookahead_topic, Float64, queue_size=10)
         self.integral_pub = rospy.Publisher(integral_topic, Float64, queue_size=10)
-
-        # listener = tf.TransformListener()
         
-
     def getRange(self, data, angle):
         # data: single message from topic /scan
         # angle: between -45 to 225 degrees, where 0 degrees is directly to the right
@@ -115,27 +111,17 @@ class WallFollow:
         angle_rad = math.radians(input_angle)
         number_of_beams = len(data.ranges)
         array_increment = angle_rad / data.angle_increment
-
-        # TODO check if in FOV
         
         array_index = (number_of_beams / 2 + array_increment if number_of_beams / 2 + array_increment <= number_of_beams else number_of_beams)
         
         result = data.ranges[int(array_index)]
         
-        #http://wiki.ros.org/tf/Tutorials/Writing%20a%20tf%20listener%20%28Python%29
-        #The tranformation from the lidar to, idk. possibly base_link, needs to be implemented properly
-        # try:
-        #     (trans, rot) = listener.lookupTransform('/base_link',data.header.frame_id,rospy.Time(0))
-        # except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-        #     # TODO continue
-        
         if (result and result > 0.0 and result < 10.0):
             return result
         else:
             # rospy.loginfo("Invalid data: " + str(result))
-            return 10.0  # TODO introduce error handling
+            return 10.0  # TODO better error handling
 
-    
     def pid_control(self, error):
         global integral
         global prev_error
@@ -150,7 +136,6 @@ class WallFollow:
         if prev_time > 0.0:
             delta_t = time - prev_time
             delta_err = error - prev_error
-            # rospy.loginfo("Invalid data: " + str(result))
             integral += delta_t * error
         else:  # ignore first time update
             delta_t = 1  # avoid division by 0
@@ -158,13 +143,13 @@ class WallFollow:
 
         angle = kp * error + kd * delta_err/delta_t + ki * integral
 
-        # Angle clipping. Probably handled by VESC anyway, but improves plot readiblity
+        # Angle clipping. Probably handled by VESC anyway, but improves plot readablity
         if(angle > MAX_STEERING_ANGLE):
             angle = MAX_STEERING_ANGLE
         elif (angle < -MAX_STEERING_ANGLE):
             angle = -MAX_STEERING_ANGLE
 
-        if False:
+        if BASIC_VELOCITY:
             if(angle > math.radians(20)):
                 velocity = 0.5
             elif (angle > math.radians(10)):
@@ -172,8 +157,7 @@ class WallFollow:
             else:
                 velocity = 1.5
         else:
-            # TODO fancier velocity heuristic (e.g. considering both angle and distance to obstacle ahead or TTC)
-            velocity = MAX_SPEED - (MAX_SPEED-MIN_SPEED)/math.degrees(MAX_STEERING_ANGLE) *  abs(math.degrees(kp) * error)
+            velocity = MAX_SPEED - (MAX_SPEED-MIN_SPEED)/math.degrees(MAX_STEERING_ANGLE) * abs(math.degrees(kp) * error)
             if velocity < MIN_SPEED:
                 velocity = MIN_SPEED
 
@@ -192,9 +176,9 @@ class WallFollow:
         error_msg.data = error
         self.error_pub.publish(error_msg)
 
-        # integral_msg = Float64()
-        # integral_msg.data = integral
-        # self.integral_pub.publish(integral_msg)
+        integral_msg = Float64()
+        integral_msg.data = integral
+        self.integral_pub.publish(integral_msg)
 
     def followLeft(self, data, leftDist):
         global velocity
@@ -202,27 +186,27 @@ class WallFollow:
 
         #Follow left wall as per the algorithm 
         dist_left = self.getRange(data, 180)
-        dist_theta = self.getRange(data, 180-theta)
+        dist_THETA = self.getRange(data, 180-THETA)
 
         # TODO error handling (invalid distances)
         
         max_dist = self.getRange(data, 0) + self.getRange(data, 180)
         
-        alpha = math.atan2((dist_theta*math.sin(math.radians(theta))), (dist_theta*math.cos(math.radians(theta))-dist_left)) - math.pi/2
+        alpha = math.atan2((dist_THETA*math.sin(math.radians(THETA))), (dist_THETA*math.cos(math.radians(THETA))-dist_left)) - math.pi/2
 
         dist_wall = dist_left*math.cos(alpha)
 
         # dynamic lookahead distance. No direct proportionality to avoid noise feedback
         if velocity < 3:
-            ld = 1.5
+            ld = LOOKAHEAD_DIST_MID
         if velocity > 5:
-            ld = 2.5
+            ld = LOOKAHEAD_DIST_FAST
         else:
-            ld = 2.0
+            ld = LOOKAHEAD_DIST_MID
 
         dist_wall_lookahead = dist_wall + ld * math.sin(-alpha)
 
-        if dynamic_distance:
+        if DYNAMIC_DISTANCE:
             if max_dist < 2 * MAX_WALL_DISTANCE:
                 leftDist = max_dist/2
             else:
@@ -248,7 +232,7 @@ class WallFollow:
     def lidar_callback(self, data):
         """ 
         """
-        error = self.followLeft(data, desired_distance_left)    
+        error = self.followLeft(data, DESIRED_DISTANCE_LEFT)    
         self.pid_control(error)
         
 
