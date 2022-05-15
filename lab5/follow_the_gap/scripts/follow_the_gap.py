8369d1030695ddec2ef804ea6cb7c2a7c45b6f07
windischbauer <johannes.windischbauer@gmail.com>
2022-05-15 11:21:28 +0200

    Change name back to follow_the_gap

--- lab5/follow_the_gap/scripts/follow_the_gap.py
+++ lab5/follow_the_gap/scripts/follow_the_gap.py
@@ -16,15 +16,15 @@ from dynamic_reconfigure.server import Server
 USE_DYNAMIC_RECONFIG = False
 
 if USE_DYNAMIC_RECONFIG:
-    from disparity_extender.cfg import GainsConfig
+    from follow_the_gap.cfg import GainsConfig
 
 # DISPARITY EXTENDER PARAMS
 BASIC_VELOCITY = False  # simple velocity scheme from the assignment sheet, otherwise more aggressive behaviour
-MAX_SPEED = rospy.get_param('/disparity_extender/max_speed', 6.3)  # m/s  (only without basic velocity)
-MIN_SPEED = rospy.get_param('/disparity_extender/min_speed', 1.7)  # m/s  (only without basic velocity)
+MAX_SPEED = rospy.get_param('/follow_the_gap/max_speed', 6.3)  # m/s  (only without basic velocity)
+MIN_SPEED = rospy.get_param('/follow_the_gap/min_speed', 1.7)  # m/s  (only without basic velocity)
 LOOKAHEAD_DIST_FAST = 3.0  # m  (if the car drives more than 5 m/s)
 LOOKAHEAD_DIST_MID = 2.25  # m  (if the car drives more than 3 m/s)
-LOOKAHEAD_DIST_SLOW = rospy.get_param('/disparity_extender/lookahead_dist_slow', 1.5) # m  (if the car drives slower than 3 m/s)
+LOOKAHEAD_DIST_SLOW = rospy.get_param('/follow_the_gap/lookahead_dist_slow', 1.5) # m  (if the car drives slower than 3 m/s)
 
 # The minimum distance that is considered a disparity
 DISPARITY = 3
@@ -43,7 +43,7 @@ velocity = 2.0
 def reconfig_callback(config, level):
     return config
 
-class DisparityExtender:
+class FollowTheGap:
     """ Implement reactive algorithm on the car """
     def __init__(self):
         rospy.loginfo("Hello from disparity_extender node")
@@ -112,8 +112,8 @@ class DisparityExtender:
         return (i / len(ranges)) * 180 - 90
 
 def main(args):
-    rospy.init_node("disparity_extender_node", anonymous=True)
-    de = DisparityExtender()
+    rospy.init_node("follow_the_gap_node", anonymous=True)
+    ftg = FollowTheGap()
 
     if USE_DYNAMIC_RECONFIG:
         srv = Server(GainsConfig, reconfig_callback)
